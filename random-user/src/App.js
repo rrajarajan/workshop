2b793064fc6c13998b808582e769c413f07eec0b
Raj Rajarajan <raj.rajan36@hotmail.com>
2023-03-16 18:41:58 +1100

    random user initial commit

---
+++ random-user/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Randomuser from './Randomuser';
+
+function App() {
+  return (
+    <div className="App">
+      <Randomuser />
+    </div>
+  );
+}
+
+export default App;
